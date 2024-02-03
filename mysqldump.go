b059182234a280be53b30a480c41db5f9b7c745f
doraemon <doraemonkey@qq.com>
2024-02-03 11:48:56 +0800

    feat: Try to keep mysqldump's behavior consistent

--- mysqldump.go
+++ mysqldump.go
@@ -181,7 +181,18 @@ func Dump(dns string, opts ...DumpOption) error {
 	buf.WriteString("-- Start Time: " + start.Format("2006-01-02 15:04:05") + "\n")
 	buf.WriteString("-- ----------------------------\n")
 	buf.WriteString("\n\n")
-	buf.WriteString("/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;\n")
+	buf.WriteString(`
+	/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+	/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+	/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+	/*!50503 SET NAMES utf8mb4 */;
+	/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+	/*!40103 SET TIME_ZONE='+00:00' */;
+	/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+	/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+	/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+	/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+	`)
 	// 连接数据库
 	db, err := sql.Open("mysql", dns)
 	if err != nil {
@@ -294,6 +305,17 @@ func Dump(dns string, opts ...DumpOption) error {
 		}
 
 	}
+	buf.WriteString(`
+	/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+	
+	/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+	/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+	/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+	/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+	/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+	/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+	`)
 
 	// 导出每个表的结构和数据
 
